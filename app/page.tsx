675b946dd19632021ba1012bee1de737c0ebe246
manhdau2006 <manhdau2006@gmail.com>
2026-07-28 19:53:16 +0700

    Initial commit: Next.js setup for LingoLearn

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: "50px", textAlign: "center", fontFamily: "sans-serif" }}>
+      <h1 style={{ color: "#3b82f6" }}>helo helo</h1>
+      <p>alo alo .</p>
+    </div>
+  );
+}
